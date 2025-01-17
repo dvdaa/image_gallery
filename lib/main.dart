7133ef9a02e35d6590b9c485d18beb76f86bbc2e
dvd <dvd.aa@web.de>
2025-01-17 10:38:30 +0100

    remove debug banner and remove stretchy overscroll behavior

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MaterialScrollBehavior().copyWith(
+        overscroll: false,
       ),
+      home: Placeholder(),
     );
   }
 }
