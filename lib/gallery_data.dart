98a7712c7c2c8bdcd9597fe83b65bfc986450b7d
dvd <dvd.aa@web.de>
2025-01-17 10:35:11 +0100

    add list of GalleryItems

---
+++ lib/gallery_data.dart
@@ -0,0 +1,54 @@
+import 'package:image_gallery/gallery_item.dart';
+import 'gen/assets.gen.dart';
+
+List<GalleryItem> galleryData = [
+  GalleryItem(
+    imageTitle: 'Bunter Basketball',
+    imageDate: DateTime(2020, 6, 3),
+    imageDescription:
+        'Ein farbenfrohes Basketballfeld im Freien. Im Zentrum steht ein Basketballkorb, dessen weißes Netz im leichten Wind schaukelt. Die Farben des Spielfelds leuchten unter der warmen Abendsonne. Vor vielen Jahren, während eines Sommerabends, trafen sich hier Freunde zu einem unvergesslichen Spiel, bei dem die Sieger den Sonnenuntergang bejubelten.',
+    imagePath: Assets.images.basketball1.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Adrenalin pur!',
+    imageDate: DateTime(2018, 2, 15),
+    imageDescription:
+        'Ein Skifahrer in voller Montur fliegt mutig über einen schneebedeckten Hügel, während der klare, blaue Himmel im Hintergrund strahlt. Die Sonne scheint hell, beleuchtet die funkelnden Schneeflocken und wirft lange Schatten auf die Piste. An einem perfekten Wintertag vor einigen Jahren, als der Schnee frisch und die Bedingungen ideal waren, führte dieser Sprung zu einem unvergesslichen Moment voller Adrenalin und Freude.',
+    imagePath: Assets.images.ski.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Achtung Kurve',
+    imageDate: DateTime(2022, 8, 29),
+    imageDescription:
+        'Eine große Gruppe Rennradfahrer legt sich synchron in eine scharfe Kurve, während sie mit beeindruckender Geschwindigkeit die Strecke entlangrasen. Die bunten Trikots der Fahrer leuchten im Sonnenschein, und ihre Konzentration ist greifbar. Vor einigen Jahren, während eines spannenden Wettkampfs an einem sonnigen Frühlingstag, markierte diese Kurve den Wendepunkt des Rennens und wurde zum Schauplatz eines packenden Duells um die Spitze.',
+    imagePath: Assets.images.bike.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Runners High',
+    imageDate: DateTime(2015, 12, 6),
+    imageDescription:
+        'Unsere acht Läufer bewegen sich hier in einer beeindruckenden Dreiecks-Formation über die Bahnen des sonnigen Sportplatzes am Mauerpark. Alle Athleten sind synchronisiert, ihre Schritte im perfekten Einklang, während sie die rote Tartanbahn unter ihren Füßen spüren. An einem kühlen Herbstmorgen, während eines intensiven Trainings, wurde diese Formation gewählt, um Teamarbeit und Präzision zu perfektionieren, und es war ein Moment, der die Entschlossenheit und den Zusammenhalt der Gruppe widerspiegelte.',
+    imagePath: Assets.images.running.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Swish',
+    imageDate: DateTime(2024, 2, 9),
+    imageDescription:
+        'Ein Foto, das von unten aufgenommen wurde, zeigt einen Basketballkorb mit einem strahlend blauen Himmel im Hintergrund. Die Sonne scheint hell, und das weiße Netz des Korbs hebt sich kontrastreich gegen das Blau ab. An einem klaren Sommernachmittag, vor vielen Jahren, wurde hier ein legendäres Spiel gespielt, bei dem der letzte Korb im goldenen Licht des Abends glänzte.',
+    imagePath: Assets.images.basketball2.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Ball Game',
+    imageDate: DateTime(2021, 10, 17),
+    imageDescription:
+        'Die Nahaufnahme zeigt einen gelb-grünen Tennisball, der auf dem glänzenden Boden der Sporthalle liegt. Die Textur des Balls ist deutlich sichtbar, während das gedämpfte Licht der Halle eine ruhige Atmosphäre schafft. An einem regnerischen Nachmittag fand hier ein spannendes Match statt, bei dem dieser Ball im entscheidenden Moment des Spiels eine wichtige Rolle spielte.',
+    imagePath: Assets.images.tennis1.path,
+  ),
+  GalleryItem(
+    imageTitle: 'Sieg in der Natur',
+    imageDate: DateTime(2020, 7, 7),
+    imageDescription:
+        'Eine beeindruckende Drohnenaufnahme zeigt ein Tennisfeld, das idyllisch mitten im Grünen liegt. Das leuchtend grüne Spielfeld kontrastiert wunderschön mit den umgebenden Bäumen und Wiesen. An einem sonnigen Sommertag vor einigen Jahren wurde hier ein Turnier ausgetragen, bei dem Spieler und Zuschauer gleichermaßen die atemberaubende Naturkulisse genossen.',
+    imagePath: Assets.images.tennis2.path,
+  ),
+];
